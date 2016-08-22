da60f95d982f7af67580209d7f51f366dd7cfb19
Jessica Appelbaum <jess.appelbaum@gmail.com>
2016-08-22 16:26:55 -0400

    Added image randomization

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ def hello():
 	facts = ["A giant panda spends around 55 perfect of its life collecting, preparing, and eating bamboo.",
 			 "Pandas have lived on earth for 2 to 3 million years.",
 			 "When a baby panda is born, it is shipped by FedEx to China to expand the gene pool.",
-			 "A group of pandas is called an \"embarassment\"",
+			 "A group of pandas is called an \"embarassment.\"",
 			 "Pandas like to eat bamboo because they have no umami taste receptors, so meat tastes bland to them.",
 			 "In ancient times, Chinese people feared pandas, and described them as metal-devouring black-and-white tapirs.",
 			 "Pandas walk with their front paws turned inward.",
 			 "Adult pandas have to eat as much as 80 pounds of bamboo every day to meet their nutritional needs.",
 			 "Unlike most bears, pandas do not hibernate.",
-			 "The panda's scientific name is Ailuropoda melanolecua, which means \"black and white cat foot\"",
+			 "The panda's scientific name is Ailuropoda melanolecua, which means \"black and white cat foot.\"",
 			 "The Chinese word for panda is \"xiongmao,\" which means \"bear cat.\"",
 			]
 
