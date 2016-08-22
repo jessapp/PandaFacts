71888db805a63e9560e2e10ee6df86226b3d8623
Jessica Appelbaum <jess.appelbaum@gmail.com>
2016-08-22 18:20:36 -0400

    Changed setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ config = {
 	'author_email': 'jess.appelbaum@gmail.com',
 	'version': '0.1',
 	'install_requires': ['nose'],
-	'packages': ['NAME'],
+	'packages': [],
 	'scripts': [],
 	'name': 'projectname'
 }
